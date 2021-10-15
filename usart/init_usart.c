31ca930835f4f4b2d7ddb92a3a5e7376483ed5ef
isakneuman <63139694+isakneuman@users.noreply.github.com>
2021-10-15 17:22:10 +0600

    kokok

--- usart/init_usart.c
+++ usart/init_usart.c
@@ -1,34 +1,39 @@
 #include "init_usart.h"
-
-static uint16_t array[buff_size];
-
+	
 void USART1_IRQHandler(void){
 	
-	if (USART1->SR & USART_SR_RXNE){
-		array[count_element++]	=		(USART1->DR);
+	if ( USART1->SR & USART_SR_RXNE ){
+		if(r_data_count == rbuff_size){
+			r_data_count	=	0;
+		}
+		r_data_position		=	r_data_count;
+		if(r_data_position==1){
+			GPIOA->BSRR = GPIO_BSRR_BS7;
+		}else{
+			GPIOA->BRR = GPIO_BRR_BR7;
+		}
+		r_data[r_data_count++]	=	USART1->DR;
 	}
-	if (USART1->SR & USART_SR_TXE){
-		if( count_element==buff_size ){
-			USART1->DR		=	sum();
-			count_element	=	0;
+	if ( USART1->SR & USART_SR_TXE	&& flag_for_TXE ){
+		GPIOA->ODR ^= GPIO_ODR_ODR7;
+		USART1->DR	=	t_data[t_data_position++];
+		if(t_data_position == t_data_count){
+			flag_for_TXE	=		FALSE;
+			t_data_position	=	t_data_count	=	0;
 		}
 	}
+	
 }
+//	END USART1_IRQHandler
 
-uint16_t	sum(void){
-	uint16_t buff = 0;
-	for(uint8_t i = 0; i<buff_size; i++){
-		USART1->DR	=	buff;
-		buff += array[i];
-	}
-	return buff;
-}
+
 
 uint16_t BRR(uint16_t BAUDR){
 	uint16_t DIV_Mantissa			=	( (SystemCoreClock/BAUDR/16)	<<4 );
 	uint16_t DIV_Fraction			=	( ((SystemCoreClock/BAUDR)%16)*16	);
 	return ( (uint16_t)(DIV_Mantissa|DIV_Fraction) );
 }
+//	END BRR
 
 void init_usart(uint16_t brr){
 
@@ -40,3 +45,6 @@ void init_usart(uint16_t brr){
 	init_gpio(GPIOA,RX_pin,INPUT_FLOAT,INPUT_MODE);
 	
 }
+//	END init_usart
+
+
--- usart/init_usart.h
+++ usart/init_usart.h
@@ -1,18 +1,34 @@
-#ifndef _USART
-#define _USART
+#ifndef _MY_USART
+#define _MY_USART
 	
 	#include "global.h"
 	
 	#define TX_pin		9
 	#define RX_pin		10
-	#define buff_size	2
 	
-	static uint8_t count_element;
+	#define	rbuff_size		16
+	#define	tbuff_size		16
+	
+	#define TRUE			1
+	#define FALSE			0
+	
+	static 	uint16_t	r_data[rbuff_size];
+	static	uint8_t 	r_data_count;
+	static	uint8_t 	r_data_position;
+	
+	static 	uint16_t	t_data[tbuff_size];
+	static	uint8_t 	t_data_count;
+	static	uint8_t 	t_data_position;
+	
+	static	uint8_t 	flag_for_TXE;
+	static	uint8_t		flag_for_RXNE;
 	
 	uint16_t BRR(uint16_t);
-	uint16_t	sum(void);
-	void init_usart(uint16_t brr);
-	void send(void);
-	void USART1_IRQHandler(void);
+	
+	void 	init_usart(uint16_t brr);
+	void	USART1_IRQHandler(void);
+	
+	void	recieve_function	(void);
+	void	transfer_function	(void);
 	
 #endif
